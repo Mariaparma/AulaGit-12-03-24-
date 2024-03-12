1c287a715494cc243f4ef4be1e33491d977f7e2f
Maria Parma <mariaparma.tds1.senai@gmail.com>
2024-03-12 08:44:25 -0300

    primeiro commit

---
+++ aulawhile2.js
@@ -0,0 +1,16 @@
+let a = 0;
+let b = 10;
+let par;
+let resultado = 0;
+
+for (let i = a; i <= b; i++) {
+    if (i % 2 == 0) {
+        if (i !=0)
+        resultado = par + resultado;
+        console.log(par)
+    }
+
+}
+
+console.log(resultado)
+
