1d01581986bc229dd6109aef5b91cb593ee5fa81
Maria Parma <mariaparma.tds1.senai@gmail.com>
2024-03-14 17:02:11 -0300

    definição da cores

--- array01.js
+++ array01.js
@@ -1,3 +1,15 @@
-let cores = [azul,preto,vermelho]
+let cores = [azul,preto,vermelho,amarelo,laranja]
 
 console.log(cores)
+
+//Exibir por posicao ou manipular por posicao.
+
+//console.log(cores[2]); //saida: azul
+
+//manipular um textinho falando que minha cor favorita é: tal cor.
+
+let mensagem = 'Minha cor favorita é: ' + cores[2];
+let mensagemPlural = 'Minhas cores favoritas são: ' + cores;
+//console.log(cores[2]);
+//console.log(mensagem);
+//console.log(mensagemPlural);
